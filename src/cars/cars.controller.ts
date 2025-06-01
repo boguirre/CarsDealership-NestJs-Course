ac17e59fd3e93fa015bf04eeaf9b6ae78049ccb2
Bruno Aguirre <boguirre@gmail.com>
2025-05-31 23:57:42 -0500

    avance

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -14,13 +15,13 @@ export class CarsController {
     }
 
     @Get(':id')
-    getCarById( @Param('id', ParseIntPipe) id: number ){
+    getCarById( @Param('id', ParseUUIDPipe) id: string ){
         return this.carsService.findOneById( id );
     }
 
     @Post()
-    createCar( @Body() body:any ){
-        return body;      
+    createCar( @Body() createCarDto: CreateCarDto ){
+        return createCarDto;      
     }
 
     @Patch (':id')
